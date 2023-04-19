0922822a699677646f567134dc0c562c68fab033
Long Li <lilong@gmail.com>
2023-04-19 12:21:15 +0900

    update zh chains path

--- pages/zh/chain/[chain].js
+++ pages/zh/chain/[chain].js
@@ -8,7 +8,7 @@ import AddNetwork from "../../../components/chain";
 import Layout from "../../../components/Layout";
 import RPCList from "../../../components/RPCList";
 import chainIds from "../../../constants/chainIds.json";
-import customChainIds from "../../constants/customChains.json"
+import customChainIds from "../../../constants/customChains.json"
 
 export async function getStaticProps({ params }) {
   const chains = await fetcher("https://chainid.network/chains.json");
